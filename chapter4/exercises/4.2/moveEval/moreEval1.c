bc11e9024773dc8632cb72c2ec812b4473d00ce3
JobaHD <92954275+JobaHD@users.noreply.github.com>
2023-12-17 15:07:42 -0500

    Done with chapter 4 exercises, now starting projects

--- chapter4/exercises/4.2/moveEval/moreEval1.c
+++ chapter4/exercises/4.2/moveEval/moreEval1.c
@@ -13,6 +13,13 @@ int main(void) {
     i += j += k;
     printf("%d %d %d\n", i, j, k);
 
+    i = 1; j = 2; k = 3;
+    i -= j -= k;
+    printf("%d %d %d\n", i, j, k);
+
+    i = 2; j = 1; k = 0;
+    i *= j *= k;
+    printf("%d %d %d\n", i, j, k);
 
     return 0;
 }
