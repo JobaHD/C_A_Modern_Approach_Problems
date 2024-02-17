e97c71ff94b68a14ac529f3a8bcd4fcdf140075c
JobaHD <92954275+JobaHD@users.noreply.github.com>
2024-02-17 14:15:46 -0500

    feat: did some more exercises.

--- chapterD/exercises/capitalize.c
+++ chapterD/exercises/capitalize.c
@@ -3,40 +3,33 @@
 #include <stdlib.h>
 
 void capitalize(char str[], int n) {
-    int i = 0;
-    while (str[i] != '\0') {
-        if(isalpha(str[i])) {
-            str[i] = toupper(str[i]);
-        }
-        i++;
-    }
+  int i = 0;
+  while (str[i] != '\0') {
+    toupper(str[i++]);
+  }
 
-    return;
+  return;
 }
 
 void capitalizePtr(char *str, int n) {
-    while (*str != '\0') {
-        if(isalpha(*str)) {
-            *str = toupper(*str);
-        }
-        str++;
-    }
+  while (*str != '\0') {
+    toupper(*str);
+    str++;
+  }
 
-    return;
+  return;
 }
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        exit(EXIT_FAILURE);
-    }
+  if (argc < 2) {
+    exit(EXIT_FAILURE);
+  }
 
-    char **n = argv;
-    while (*++n != NULL) {
-        capitalizePtr(*n, 100);
-        printf("%s\n", *n);
-    }
+  char **n = argv;
+  while (*++n != NULL) {
+    capitalize(*n, 100);
+    printf("%s\n", *n);
+  }
 
-
-    exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
-
---
+++ chapterD/exercises/censor.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int censor(char *str, int n) {
+    char *currChar = str;
+    while (currChar < str + n) {
+        if (tolower(*currChar) == "
+
+}
+
+int main(int argc, char **argv) {
+}
